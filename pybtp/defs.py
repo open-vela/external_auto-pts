michal-narajowski/auto-pts

btp: Add Set MITM command

pybtp/defs.py
@@ -86,6 +86,7 @@ GAP_CONN_PARAM_UPDATE = 0x16
 GAP_OOB_LEGACY_SET_DATA = 0x17
 GAP_OOB_SC_GET_LOCAL_DATA = 0x18
 GAP_OOB_SC_SET_REMOTE_DATA = 0x19
+GAP_SET_MITM = 0x1a
 GAP_EV_NEW_SETTINGS = 0x80
 GAP_DEVICE_FOUND_FLAG_RSSI = 0x01
 GAP_DEVICE_FOUND_FLAG_AD = 0x02

open-vela/external_auto-pts

pybtp/defs.py
@@ -87,6 +87,7 @@ GAP_PAIRING_CONSENT_RSP = 0x17
 GAP_OOB_LEGACY_SET_DATA = 0x18
 GAP_OOB_SC_GET_LOCAL_DATA = 0x19
 GAP_OOB_SC_SET_REMOTE_DATA = 0x1a
+GAP_SET_MITM = 0x1b
 GAP_EV_NEW_SETTINGS = 0x80
 GAP_DEVICE_FOUND_FLAG_RSSI = 0x01
 GAP_DEVICE_FOUND_FLAG_AD = 0x02