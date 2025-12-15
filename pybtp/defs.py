michal-narajowski/auto-pts

common: Add support for OOB legacy & secure connections

pybtp/defs.py
@@ -83,6 +83,9 @@ GAP_PASSKEY_ENTRY = 0x13
 GAP_PASSKEY_CONFIRM = 0x14
 GAP_START_DIRECT_ADV = 0x15
 GAP_CONN_PARAM_UPDATE = 0x16
+GAP_OOB_LEGACY_SET_DATA = 0x17
+GAP_OOB_SC_GET_LOCAL_DATA = 0x18
+GAP_OOB_SC_SET_REMOTE_DATA = 0x19
 GAP_EV_NEW_SETTINGS = 0x80
 GAP_DEVICE_FOUND_FLAG_RSSI = 0x01
 GAP_DEVICE_FOUND_FLAG_AD = 0x02

open-vela/external_auto-pts

pybtp/defs.py
@@ -84,6 +84,9 @@ GAP_PASSKEY_CONFIRM = 0x14
 GAP_START_DIRECT_ADV = 0x15
 GAP_CONN_PARAM_UPDATE = 0x16
 GAP_PAIRING_CONSENT_RSP = 0x17
+GAP_OOB_LEGACY_SET_DATA = 0x18
+GAP_OOB_SC_GET_LOCAL_DATA = 0x19
+GAP_OOB_SC_SET_REMOTE_DATA = 0x1a
 GAP_EV_NEW_SETTINGS = 0x80
 GAP_DEVICE_FOUND_FLAG_RSSI = 0x01
 GAP_DEVICE_FOUND_FLAG_AD = 0x02